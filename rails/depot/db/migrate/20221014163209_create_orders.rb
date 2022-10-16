6664e5724ae13a9ff62aaa0a38097eb6f0d1ca2c
mirko1593 <mirko1593@hotmail.com>
2022-10-17 00:29:29 +0800

    depot: Task G Capturing an Order

---
+++ rails/depot/db/migrate/20221014163209_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.text :address
+      t.string :email
+      t.integer :pay_type
+
+      t.timestamps
+    end
+  end
+end
