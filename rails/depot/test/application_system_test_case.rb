d5b607ce6dd360a7e582b650cd97c668a5becffe
mirko1593 <mirko1593@hotmail.com>
2022-10-18 00:09:26 +0800

    depot Task I Logging In

--- rails/depot/test/application_system_test_case.rb
+++ rails/depot/test/application_system_test_case.rb
@@ -1,5 +1,9 @@
 require "test_helper"
 
+class ActionDispatch::SystemTestCase
+  include AuthenticationHelpers
+end
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
 end
