86522ba59bd414500862022b9b4a0dfbaee9b032
mirko1593 <mirko1593@hotmail.com>
2022-10-17 22:28:57 +0800

    depot: Task H Sending Confirmation Emails

--- rails/depot/db/migrate/20221014163209_create_orders.rb
+++ rails/depot/db/migrate/20221014163209_create_orders.rb
@@ -1,11 +1,11 @@
 class CreateOrders < ActiveRecord::Migration[7.0]
   def change
-    # create_table :orders do |t|
-    #   t.text :address
-    #   t.string :email
-    #   t.integer :pay_type
+    create_table :orders do |t|
+      t.text :address
+      t.string :email
+      t.integer :pay_type
 
-    #   t.timestamps
-    # end
+      t.timestamps
+    end
   end
 end
