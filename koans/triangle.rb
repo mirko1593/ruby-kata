b60f212d5f6f0c417b26e12b36b1503b41e43b0d
mirko1593 <mirko1593@hotmail.com>
2022-10-26 02:18:54 +0800

    work for ruby koan

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,6 +14,17 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  if a <= 0 || b <= 0 || c <= 0 || (a + b) <= c || a + c <= b || b + c <= a
+    raise TriangleError
+  end
+
+  if a == b && a == c && b == c
+    :equilateral
+  elsif a == b || a == c || b == c
+    :isosceles
+  else
+    :scalene
+  end
   # WRITE THIS CODE
 end
 
